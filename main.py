1b1733c41f1ae026352b07ff710a9e3a3bbe3f24
ratanparmar <parmar.ratan@gmail.com>
2019-06-04 00:38:54 +0530

    initial project

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from articlebag import Articles
+
+Articles = Articles()
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html',articles=Articles)
+
+@app.route('/article/<string:id>/')
+def article(id):
+    return render_template('article.html',id=id)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
